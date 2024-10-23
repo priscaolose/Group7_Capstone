a26946dfd3bae54865df7c43060f8d015db1d5b8
Genesis Umanzor <123410434+GeneUmanzor@users.noreply.github.com>
2024-10-23 15:14:39 -0500

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,34 @@
+//src/auth.js
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signout } from "firebase/auth";
+import app from './firebaseConfig';
+
+const auth = getAuth(app);
+
+//signup func
+export const signup = async (email, password) => {
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+    return userCrendential.user;
+  } catch (error) {
+    console.error("Error signing up:", error.message);
+  }
+};
+
+//login func
+export const login = async (email, password) => {
+  try {
+    const userCredential = await signInWithEmailAndPassword(auth, email, password);
+    return userCredential.user;
+  } catch (error) {
+    console.error("Error logging in:", error.message);
+  }
+};
+
+//logout func
+export const logout = async () => {
+  try {
+    await signOut(auth);
+  } catch (error) {
+    console.error("Error signing out:", error.message);
+  }
+};
