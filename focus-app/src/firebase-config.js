a2157bcf7219c7d092b06de1436e77f4c3076dc0
William-Harrison4 <157984139+William-Harrison4@users.noreply.github.com>
2024-10-29 17:52:30 -0500

    Update firebase-config.js

--- focus-app/src/firebase-config.js
+++ focus-app/src/firebase-config.js
@@ -1,11 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "src/App";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
