0fc7f3bdac4d4d042f85e2bc773da57c4763b079
William Harrison <williamharrison2@my.unt.edu>
2024-10-29 19:36:35 -0500

    Added the push method to generate a userID via a firebase key.

--- focus-app/api/register.js
+++ focus-app/api/register.js
@@ -1,13 +1,15 @@
 import { db } from '../src/firebase-config';
-import { ref, set } from 'firebase/database';
+import { ref, set, push } from 'firebase/database';
 
 export default async function handler(req, res) {
   if(req.method === 'POST') {
     const { fistName, lastName, email, userName, password } = req.body;
+    const userRef = push(ref(db, 'Users'));
 
-    set(ref(db, 'Users/' + userName), {
+    set(userRef, {
       fistName,
       lastName,
+      userName,
       email,
       password,
     });
