0e98f75ce50c6cfd09ad9a01b7d697e84d1ba415
William Harrison <williamharrison2@my.unt.edu>
2024-11-09 13:50:54 -0600

    Testing

--- focus-app/api/register.js
+++ focus-app/api/register.js
@@ -1,18 +1,28 @@
-import { db } from '../../src/firebase-config';
-import { collection, setDoc, doc } from 'firebase/firestore';
+import { db } from "../../src/firebase-config";
+import { collection, setDoc, doc } from "firebase/firestore";
 
 export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    const { firstName, lastName, email, userName, phonenumber, password } = req.body;
-    const productsRef = collection(db, 'Users');
+  if (req.method === "POST") {
+    const { firstName, lastName, email, userName, phonenumber, password } =
+      req.body;
+    const userRef = collection(db, "Users");
+
+    console.log("Trying to add user");
 
     try {
-      await setDoc(doc(productsRef), { firstName, lastName, email, phonenumber, password, username: userName });
-      res.status(200).json({ message: 'User registered!' });
+      await setDoc(doc(userRef), {
+        firstName,
+        lastName,
+        email,
+        phonenumber,
+        password,
+        username: userName,
+      });
+      res.status(200).json({ message: "User registered!" });
     } catch (error) {
-      res.status(500).json({ error: 'Failed to register' });
+      res.status(500).json({ error: "Failed to register" });
     }
   } else {
-    res.status(405).json({ error: 'Method not allowed' });
+    res.status(405).json({ error: "Method not allowed" });
   }
 }
