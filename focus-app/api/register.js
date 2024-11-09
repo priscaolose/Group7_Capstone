5ec18212782e8926cb4f097071bb6dac88723451
William Harrison <williamharrison2@my.unt.edu>
2024-11-09 13:42:49 -0600

    Made updates to the register api

--- focus-app/api/register.js
+++ focus-app/api/register.js
@@ -1,60 +1,18 @@
-import { db } from '../src/firebase-config';
+import { db } from '../../src/firebase-config';
 import { collection, setDoc, doc } from 'firebase/firestore';
-const express = require('express');
-const app = express();
 
-app.use(express.json());
-
-app.post('/registration', (req, res) => {
-  console.log('Inside registration api');
-  const { firstName, lastName, email, userName, phonenumber, password } = req.body;
-  const productsRef = collection(db, 'Users');
-
-    try{
-      setDoc(doc(productsRef), {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phonenumber: phonenumber,
-        password: password,
-        username: userName,
-      });
-      res.json({ message: 'User registered!' });
-    } catch (error)
-    {
-      res.json({ error: 'Failed to register '})
-    }
-});
-
-/*export default async function handler(req, res) {
-  if(req.method === 'POST') {
+export default async function handler(req, res) {
+  if (req.method === 'POST') {
     const { firstName, lastName, email, userName, phonenumber, password } = req.body;
     const productsRef = collection(db, 'Users');
 
-    try{
-      await setDoc(doc(productsRef), {
-        firstName: firstName,
-        lastName: lastName,
-        email: email,
-        phonenumber: phonenumber,
-        password: password,
-        username: userName,
-      });
-
     try {
-      userRef.set( {
-        firstName,
-        lastName,
-        userName,
-        email,
-        phonenumber,
-        password,
-      });
-  
-      res.json({ message: 'User registered!' });
-    } catch (error)
-    {
-      res.json({ error: 'Failed to register '})
+      await setDoc(doc(productsRef), { firstName, lastName, email, phonenumber, password, username: userName });
+      res.status(200).json({ message: 'User registered!' });
+    } catch (error) {
+      res.status(500).json({ error: 'Failed to register' });
     }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
   }
-}*/
+}
