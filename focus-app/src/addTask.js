e3d528a41b286023cb5348d8c8fc608a521089ef
Bernice <uchechichiaha@gmail.com>
2024-11-23 12:24:00 -0600

    Created a basci functional addTask page

--- focus-app/src/addTask.js
+++ focus-app/src/addTask.js
@@ -2,15 +2,25 @@ import React, { useState } from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
 import { useNavigate } from 'react-router-dom';
+import { makeStyles } from '@mui/styles';  // Correct import
+import { Grid2, Paper } from '@mui/material';
+const useStyles = makeStyles((theme) =>({
+    Grid2: {
+        width : '100%',
+        margin: '0px;'
+    }
+}));
 
 const AddTask = (email) =>{
     const navigate = useNavigate();
+    const classes = useStyles();
     return (
-        <div className="mainContainer"> 
+
+        <Grid2 container spacing ={2}className = {classes.Grid2}> 
         <Header />
             Add Task
         <Footer />
-        </div>
+        </Grid2>
     )
 }
 
