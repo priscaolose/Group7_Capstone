48f6b43e8942ad0037628ce318e6bf9bba775423
priscaolose <68451382+priscaolose@users.noreply.github.com>
2024-11-24 22:47:51 -0600

    re-created dashboard page

--- focus-app/src/App.js
+++ focus-app/src/App.js
@@ -2,14 +2,29 @@ import React from 'react';
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Header';
+import Footer from './components/Footer';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
+
+// Create a theme with custom typography
+const theme = createTheme({
+  typography: {
+    fontFamily: '"Poppins", sans-serif', // Set the default font for Typography
+  },
+});
 
 function App() {
   return (
     <div className="App">
+      <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Ensures consistent baseline styling across the app */}
       <Header />
       <Dashboard />
+    </ThemeProvider>
+      <Footer />
     </div>
   );
 }
+
 
 export default App;
