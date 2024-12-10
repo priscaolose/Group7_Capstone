007d6e609e7c87dc3facadfaefc3d208f45173ca
priscaolose <68451382+priscaolose@users.noreply.github.com>
2024-12-09 20:55:34 -0600

    fixed build issues

--- focus-app/src/Components/Dashboard.js
+++ focus-app/src/Components/Dashboard.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import { Box, Typography, Paper, useMediaQuery } from '@mui/material';
+import {
+  Box,
+  Typography,
+  Paper,
+  useMediaQuery,
+} from '@mui/material';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { auth } from '../firebase/firebaseConfig';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom'; // Import useLocation for accessing state
+import { Link } from 'react-router-dom';
 
+// Custom Theme
 const theme = createTheme({
   typography: {
-    fontFamily: '"Roboto", sans-serif',
+    fontFamily: '"Poppins", sans-serif', // Use Poppins as the font
     h5: {
       fontWeight: 600,
     },
@@ -22,7 +32,7 @@ const theme = createTheme({
   },
   palette: {
     primary: {
-      main: '#1a73e8',
+      main: '#1a73e8', // the blue you had before
     },
     background: {
       default: '#f4f6f8',
@@ -31,101 +41,215 @@ const theme = createTheme({
 });
 
 function Dashboard() {
-  const location = useLocation();
+  const location = useLocation(); // Access location state
   const isSmallScreen = useMediaQuery('(max-width: 900px)');
   const [userFirstName, setUserFirstName] = useState(null);
   const [user, setUser] = useState(null);
 
-  // Fetch user data from backend API
-  const getUserData = async (userID) => {
-    try {
-      const response = await fetch(`/api/getUser?userID=${userID}`);
-      const data = await response.json();
-      if (data.name) {
-        setUserFirstName(data.name);
-      } else {
-        console.error('Error fetching user data:', data.error);
-      }
-    } catch (error) {
-      console.error('Error fetching user data:', error);
-    }
-  };
-
   useEffect(() => {
-    if (location.state && location.state.firstName) {
-      setUserFirstName(location.state.firstName);
-    }
+    const fetchUserData = async (userID) => {
+      try {
+        const response = await fetch(`/api/getUser?userID=${userID}`);
+        if (response.ok) {
+          const data = await response.json();
+          setUserFirstName(data.name); // Update state with the user's first name
+        } else {
+          console.error('Failed to fetch user data:', response.statusText);
+          setUserFirstName('Guest'); // Fallback if the API call fails
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+        setUserFirstName('Guest'); // Fallback in case of error
+      }
+    };
 
     const unsubscribe = onAuthStateChanged(auth, (loggedInUser) => {
       if (loggedInUser) {
         setUser(loggedInUser);
-        getUserData(loggedInUser.uid); // Fetch user data when logged in
+        fetchUserData(loggedInUser.uid); // Fetch user data from the API using the user's ID
       } else {
         setUser(null);
+        setUserFirstName('Guest');
       }
     });
 
     return () => unsubscribe();
-  }, [location.state]);
+  }, []);
 
   return (
-    <div className="mainContainer">
-      <Header />
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'flex-start',
-          minHeight: '100vh',
-          px: isSmallScreen ? 2 : 4,
-          py: 1,
-          backgroundColor: 'white',
-          overflowY: 'auto',
-        }}
-      >
+    <ThemeProvider theme={theme}>
+      <div className='mainContainer'>
+        <Header />
         <Box
           sx={{
-            display: 'grid',
-            gridTemplateColumns: { xs: '1fr', md: 'repeat(3, 1fr)' },
-            gap: 4,
-            maxWidth: '1200px',
-            width: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+            justifyContent: 'flex-start', // Adjust to start from the top
+            minHeight: '100vh',
+            px: isSmallScreen ? 2 : 4,
+            py: 1,
+            backgroundColor: 'white',
+            overflowY: 'auto', // Enables scrolling when content exceeds viewport
           }}
         >
-          <Box sx={{ display: 'grid', gap: 4 }}>
-            <Paper
-              sx={{
-                p: 3,
-                borderRadius: '16px',
-                background: 'linear-gradient(#FFF1F1, #E2EAF1)',
-                boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
-              }}
-            >
-              <Typography
-                variant="h6"
-                sx={{ color: theme.palette.primary.main, fontWeight: 'bold', textAlign: 'left' }}
-              >
-                Welcome Back
-              </Typography>
-              <Typography
-                variant="h5"
+          {/* Main Content */}
+          <Box
+            sx={{
+              display: 'grid',
+              gridTemplateColumns: { xs: '1fr', md: 'repeat(3, 1fr)' },
+              gap: 4,
+              maxWidth: '1200px',
+              width: '100%',
+            }}
+          >
+            {/* Left Column */}
+            <Box sx={{ display: 'grid', gap: 4 }}>
+              {/* Welcome Section */}
+              <Paper
                 sx={{
-                  mt: 1,
-                  color: '#333',
-                  fontWeight: 'bold',
-                  textAlign: 'left',
+                  p: 3,
+                  borderRadius: '16px',
+                  background: 'linear-gradient(#FFF1F1, #E2EAF1)',
+                  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
                 }}
               >
-                {user ? userFirstName : 'Guest'}
-              </Typography>
-            </Paper>
-            {/* More content here */}
+                <Typography variant="h6" sx={{ color: theme.palette.primary.main, fontWeight: 'bold', textAlign: 'left' }}>
+                  Welcome Back
+                </Typography>
+                <Typography
+                  variant="h5"
+                  sx={{
+                    mt: 1,
+                    color: '#333',
+                    fontWeight: 'bold',
+                    textAlign: 'left',
+                  }}
+                >
+                  {userFirstName}
+                </Typography>
+              </Paper>
+
+              {/* Today's Tasks Section */}
+              <Paper
+                sx={{
+                  p: 3,
+                  borderRadius: '16px',
+                  background: 'linear-gradient(#FFF1F1, #E2EAF1)',
+                  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                  minHeight: '50vh',
+                }}
+              >
+                <Typography variant="h6" sx={{ color: theme.palette.primary.main }}>
+                  Today's Tasks
+                </Typography>
+              </Paper>
+            </Box>
+
+            {/* Center Column */}
+            <Box sx={{ display: 'grid', gap: 4 }}>
+              <Paper
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  borderRadius: '16px',
+                  background: 'linear-gradient(#FFF1F1, #E2EAF1)',
+                  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                  p: 4,
+                }}
+                component={Link} // Link to the Add Task page
+                to="/add-task" // Route to navigate to  Add Task page
+              >
+                <Typography
+                  variant="h3"
+                  sx={{
+                    color: '#333',
+                    fontWeight: '300',
+                  }}
+                >
+                  00:00
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    color: theme.palette.primary.main,
+                    fontSize: '1.25rem',
+                    mt: 2,
+                  }}
+                >
+                  Add task here
+                </Typography>
+              </Paper>
+              <Paper
+                sx={{
+                  p: 3,
+                  borderRadius: '16px',
+                  background: 'linear-gradient(#FFF1F1, #E2EAF1)',
+                  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                }}
+              >
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateCalendar />
+                </LocalizationProvider>
+              </Paper>
+            </Box>
+
+            {/* Right Column */}
+            <Box sx={{ display: 'grid', gap: 4 }}>
+              <Paper
+                sx={{
+                  p: 3,
+                  borderRadius: '16px',
+                  background: 'linear-gradient(#FFF1F1, #E2EAF1)',
+                  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: theme.palette.primary.main,
+                  }}
+                >
+                  Reminder:
+                </Typography>
+                <Typography
+                  variant="body1"
+                  sx={{
+                    color: '#333',
+                    mt: 1,
+                    lineHeight: 1.5,
+                  }}
+                >
+                  It's always a great time to take the first step.
+                </Typography>
+              </Paper>
+              <Paper
+                sx={{
+                  p: 3,
+                  borderRadius: '16px',
+                  background: 'linear-gradient(#FFF1F1, #E2EAF1)',
+                  boxShadow: '0 4px 12px rgba(0, 0, 0, 0.1)',
+                  minHeight: '50vh',
+                }}
+              >
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: theme.palette.primary.main,
+                    mb: 2,
+                  }}
+                >
+                  Notes
+                </Typography>
+              </Paper>
+            </Box>
           </Box>
         </Box>
-      </Box>
-      <Footer />
-    </div>
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
 
