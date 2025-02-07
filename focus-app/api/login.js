286124eb5a2d99beea4b054c1d90c5334a967444
William Harrison <williamharrison2@my.unt.edu>
2025-02-07 13:16:14 -0600

    added error detection

--- focus-app/api/login.js
+++ focus-app/api/login.js
@@ -29,7 +29,7 @@ app.get("/api/login", async (req, res) => {
     console.log("Name: " + userData.firstName);
     res.status(200).json({ name: userData.firstName });
   } catch (error) {
-    res.json({ error: "Failed to sign in" });
+    res.json({ error: "Failed to sign in:" + error });
   }
 });
 
