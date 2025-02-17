c698eb6af821ad6ca63c3ba02141eca97f928de6
Genesis Umanzor <123410434+GeneUmanzor@users.noreply.github.com>
2025-02-16 20:30:11 -0600

    Create server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const popoutRoutes = require('./routes/popoutRoutes');
+const admin = require('firebase-admin');
+
+//ini. firebase admin SDK
+const serviceAccount = require('./firebaseServiceAccount.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL:
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use('/api/popout', popoutRoutes); //attach pop out window routes
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
